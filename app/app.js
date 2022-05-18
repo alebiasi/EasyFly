9cf01204c41b270c6b2c97fe45bb3ef7ae8ed9e4
NicolaVisona <nicola.visona@studenti.unitn.it>
2022-05-18 10:22:48 +0200

    More merge conflit resolved

--- app/app.js
+++ app/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-<<<<<<< HEAD
 const checkin = require("./checkin.js");
 const tokenchecker = require("./tokenchecker.js");
 const path = require('node:path');
+const flights = require("./flights.js"); //checkin.js
 /**
  * configure parsing middleware
  */
@@ -13,8 +13,14 @@ app.use(express.urlencoded({extended: true}));
 /**
  * expose static resources
  */
-app.use("/",express.static("static"));
+app.use("/", express.static(process.env.FRONTEND || 'static'));
+app.use('/', express.static('static')); // expose also this folder
 app.use("/style",express.static("style"));
+
+app.use((req,res,next) => {
+    console.log(req.method + ' ' + req.url)
+    next()
+})
 
 /**
  * authentication middleware
@@ -37,29 +43,13 @@ app.use("/accept_page",function(req,res){
     var mypath = path.join(__dirname,"../html_checkin/accept_page.html");
     res.sendFile(mypath);
 });
+
 app.use("/api/v1/requests",checkin);
-
-//insert page not found
-=======
-const flights = require("./flights.js"); //checkin.js
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.use("/", express.static(process.env.FRONTEND || 'static'));
-app.use('/', express.static('static')); // expose also this folder
-
-app.use((req,res,next) => {
-    console.log(req.method + ' ' + req.url)
-    next()
-})
-
 app.use("/api/v1/flights", flights);
 
 app.use((req, res) => {
     res.status(404);
     res.json({ error: '404: Not found' });
 });
->>>>>>> 50f0edc2074cacb5663112f58ae5102e8b41851b
 
 module.exports = app;
