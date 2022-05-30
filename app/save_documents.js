80a02b83b839c89e658dfec2077bff11e76abe59
NicolaVisona <nicola.visona@studenti.unitn.it>
2022-05-30 16:18:45 +0200

    Add user notification when checkin request is accepted

--- app/save_documents.js
+++ app/save_documents.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 const Document = require("./models/document")
 const formidable = require('formidable');
 const router = express.Router();
-
+const nodemailer = require("nodemailer");
 
 function makeid(length) {
     var text = "";
@@ -79,6 +79,29 @@ router.post('/', async (req, res) => {
     var form = new formidable.IncomingForm();
 
     form.parse(req, function (err, fields, files) {
+        if(fields.document_type==2){    //if document_type == 2 it means that i am inserting the boarding card, so i can notify the user that the check-in procedure went well with a mail
+            var transporter = nodemailer.createTransport({  //create transporter to send email to users
+                service: 'gmail',
+                auth: {
+                    user: 'easyfly.ids2@gmail.com',
+                    pass: process.env.EMAIL_PASS
+                }
+            });
+            var mailOptions = { //create email
+                from: 'easyfly.ids2@gmail.com',
+                to: fields.email, 
+                subject: 'Esito richiesta check-in online',
+                html: "<p>Il team di EasyFly è lieta di informarla che la sua richiesta di check-in online è andata a buon fine ed è stata accettata.</p><p>Le auguriamo una buona giornata ed un buon volo</p><p>Il team di EasyFly</p>"
+            };
+            transporter.sendMail(mailOptions, function(error, info){    //send email
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
+        }
+        
         var oldpath = files.filetoupload.filepath
         let id = makeid(16);
         let src = files.filetoupload.originalFilename;
